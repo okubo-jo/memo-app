b9f528761564d573e254992e3fe870bc43381839
okubo-jo <jtk1028@icloud.com>
2026-03-23 15:21:28 +0900

    メモの編集機能を追加

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def index():
 
     return render_template("index.html", memos=memos)
 
+#削除機能
 @app.route("/delete/<int:index>")
 def delete(index):
 
@@ -30,6 +31,25 @@ def delete(index):
         f.writelines(memos)
 
     return redirect("/")
+
+#編集機能
+@app.route("/edit/<int:index>", methods=["GET", "POST"])
+def edit(index):
+
+    with open("memo.txt", "r", encoding="utf-8") as f:
+        memos = f.readlines()
+
+    if request.method == "POST":
+        new_memo = request.form["memo"]
+        memos[index] = new_memo + "\n"
+
+        with open("memo.txt", "w", encoding="utf-8") as f:
+            f.writelines(memos)
+
+        return redirect("/")
+
+    memo = memos[index]
+    return render_template("edit.html", memo=memo, index=index)
 
 if __name__ == "__main__":
     app.run(debug=True)
